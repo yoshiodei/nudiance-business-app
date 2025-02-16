98d8a427608e433cd580892d19c76d3931ffde0d
Yoshi Odei <yoshiodei@yahoo.com>
2025-02-16 13:08:01 +0000

    fetch firebase data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,23 @@
-export default function Home() {
-    return (
+import { db } from "@/firebase/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+// import { useRouter } from "next/navigation";
+
+async function getJobs() {
+  const querySnapshot = await getDocs(collection(db, "jobList"));
+  return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+}
+
+export default async function Home() {
+  
+  const jobs = await getJobs();
+  console.log('my job lists', jobs);
+  
+
+  return (
+    <>  
       <h1 className="text-3xl text-red-400">Hello Everyone!!</h1>
-    );
-  }
+      {/* <button onClick={ () => router.push('/login') }>Continue</button> */}
+    </>   
+  );
+}
   
--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,9 +1,13 @@
-'use client';
+// 'use client';
 
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+
+console.log('api key', process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log('auth Domain', process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,6 +18,7 @@ const firebaseConfig = {
   appId: '1:897386202691:web:8ec147d26f9ca63777f6f6',
   measurementId: 'G-E54BY8C0GE',
 };
+
 
 // Initialize Firebase only if it hasn't been initialized already
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
