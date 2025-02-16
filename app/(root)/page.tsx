cfeaa8f63f67f6790c683cd38e822839fce71210
Yoshi Odei <yoshiodei@yahoo.com>
2025-02-16 11:52:48 +0000

    create auth route

---
+++ app/(root)/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+    return (
+      <h1 className="text-3xl text-red-400">Hello Everyone!!</h1>
+    );
+  }
+  
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <h1 className="text-3xl text-red-400">Hello Everyone!!</h1>
-  );
-}
